ca53d295584fd0e65fcdd2d09472f9166e1f372e
bsm2 <basmatalaat2@gmail.com>
2024-09-28 00:29:50 +0300

    feat: valid login

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -34,3 +34,21 @@ def _hash_password(password: str) -> bytes:
     """
     pwd = password.encode()
     return bcrypt.hashpw(pwd, bcrypt.gensalt())
+
+def valid_login(self, email: str, password: str) -> bool:
+    """validate login
+
+    Args:
+        email (str): user email
+        password (str): user password
+
+    Returns:
+        bool: user exists
+    """
+    try:
+        user = self._db.find_user_by(email=email)
+    except NoResultFound:
+        return False
+    if not bcrypt.checkpw(password.encode(), user.hashed_password):
+        return False
+    return True
