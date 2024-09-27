22712b863a3f1da88dddea2ae6b40537a71116b8
bsm2 <basmatalaat2@gmail.com>
2024-09-27 23:37:00 +0300

    feat: db

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """DB module
 """
-
 from sqlalchemy import create_engine,tuple_
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -18,7 +17,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
