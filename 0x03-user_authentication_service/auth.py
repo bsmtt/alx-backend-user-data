e54fb9552f3dde07ecc30b6a38273d5274d94c2d
bsm2 <basmatalaat2@gmail.com>
2024-09-28 00:53:08 +0300

    feat: valid login

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -43,6 +43,25 @@ class Auth:
             return False
         return True
 
+    def create_session(self, email: str) -> str:
+        """Returns user session ID
+
+        Args:
+            email (str): user email
+
+        Returns:
+            str: user session uuid
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return None
+
+        session_id = _generate_uuid()
+        self._db.update_user(user.id, session_id=session_id)
+
+        return session_id
+
 
 def _hash_password(password: str) -> bytes:
     """ Creates password hash
