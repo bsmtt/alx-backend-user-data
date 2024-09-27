baa13505e012b25e503fefa7c88bbebafa90e8ea
bsm2 <basmatalaat2@gmail.com>
2024-09-28 01:05:56 +0300

    feat: valid login

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -30,3 +30,17 @@ def register_user() -> str:
         return jsonify({"email": email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route("/sessions", methods=["POST"])
+def login() -> str:
+    email = request.form['email']
+    password = request.form['password']
+    if not AUTH.valid_login(email, password):
+        abort(401)
+    
+    session_id = AUTH.create_session(email)
+    response = jsonify({"email": email, "message": "logged in"})
+    response.set_cookie("session_id", session_id)
+    
+    return response
